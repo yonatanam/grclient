a0670d8b14b52b9ec9853e6274f56db08265ab72
Avi <Avi@DESKTOP-D4DV7J3>
2016-12-16 16:00:48 +0200

    GUI changes

--- src/gui/ClientGUI.java
+++ src/gui/ClientGUI.java
@@ -1,11 +1,18 @@
 package gui;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 public class ClientGUI extends JFrame{
 
@@ -19,39 +26,155 @@ public class ClientGUI extends JFrame{
 	private JLabel LabelHost = null;
 	private JTextField TextHost = null;
 	private JTextField TextPort = null;
+	private JTextField nullField = null;
 	private JButton ButtonOk = null;
 	private JButton ButtonCancel = null;
 
 	public ClientGUI() {
 		getContentPane().setLayout(null);
-
-		LabelPort = new JLabel("Port");
+		
+				nullField = new JTextField();
+				nullField.setOpaque(false);     
+				getContentPane().add(nullField);
+		
+				//Hostname label		
+				JLabel HostName = new JLabel();
+				HostName.setBounds(71, 9, 229, 40);
+				Image img1 = new ImageIcon(this.getClass().getResource("/passSmall.png")).getImage();
+				HostName.setIcon(new ImageIcon(img1));
+				getContentPane().add(HostName);
+				
+				
+				//Hostname JTextField
+				TextHost = new JTextField()
+				{
+				    @Override public void setBorder(Border border) {   //Disable field's border
+				        // No!
+				    }
+				};
+				
+				TextHost.setForeground(Color.WHITE);
+				TextHost.setHorizontalAlignment(JTextField.CENTER);
+				TextHost.setFont(new Font("Arial", Font.PLAIN, 13));
+				TextHost.setOpaque(false);       //Make Field transparent 
+				TextHost.setText("localhost");
+				TextHost.addFocusListener(new FocusAdapter() {
+					@Override
+					public void focusGained(FocusEvent e) {
+						TextHost.setText("");                //Clear "Username String" on focus
+					}
+				});
+				
+				TextHost.setToolTipText("");
+				TextHost.setColumns(10);
+				TextHost.setBounds(77, 20, 210, 23);
+				getContentPane().add(TextHost);
+				//Hostname JTextField END
+				
+				
+				
+				
+				//Port label		
+				JLabel Port = new JLabel();
+				Port.setBounds(71, 40, 229, 40);
+				Image img2 = new ImageIcon(this.getClass().getResource("/passSmall.png")).getImage();
+				Port.setIcon(new ImageIcon(img2));
+				getContentPane().add(Port);
+				//Port label END
+				
+				
+				
+				
+				//Port JTextField
+				TextPort = new JTextField()
+				{
+				    @Override public void setBorder(Border border) {   //Disable field's border
+				        // No!
+				    }
+				};
+				
+				TextPort.setForeground(Color.WHITE);
+				TextPort.setHorizontalAlignment(JTextField.CENTER);
+				TextPort.setFont(new Font("Arial", Font.PLAIN, 13));
+				TextPort.setOpaque(false);       //Make Field transparent 
+				TextPort.setText("5555");
+				TextPort.addFocusListener(new FocusAdapter() {
+					@Override
+					public void focusGained(FocusEvent e) {
+						TextPort.setText("");                //Clear "Port String" on focus
+					}
+				});
+				
+				TextPort.setToolTipText("");
+				TextPort.setColumns(10);
+				TextPort.setBounds(77, 50, 210, 23);
+				getContentPane().add(TextPort);
+				
+				
+				
+				
+		/*LabelPort = new JLabel("Port");
 		LabelPort.setBounds(10, 40, 46, 14);
 		getContentPane().add(LabelPort);
 
-		LabelHost = new JLabel("Host");
-		LabelHost.setBounds(10, 11, 46, 14);
+		LabelHost = new JLabel("localhost");
+		LabelHost.setBounds(32, 83, 222, 14);
 		getContentPane().add(LabelHost);
+		*/
 
-		getContentPane().add(getTextHost());
-		getContentPane().add(getTextPort());
+		
+		//getContentPane().add(getTextPort());
 
+		
+		//Ok button		
 		ButtonOk = new JButton("OK");
-		ButtonOk.setBounds(77, 79, 89, 23);
+		ButtonOk.setBounds(71, 86, 99, 23);
+		ButtonOk.setFont(new Font("Arial Black", Font.PLAIN, 13));
+		Image imgOK = new ImageIcon(this.getClass().getResource("/buttSmall.png")).getImage();
+		ButtonOk.setIcon(new ImageIcon(imgOK));
+		ButtonOk.setOpaque(false);
+		ButtonOk.setContentAreaFilled(false);
+		ButtonOk.setBorderPainted(false);
+		ButtonOk.setHorizontalTextPosition(JButton.CENTER);
+		ButtonOk.setVerticalTextPosition(JButton.CENTER);
+		ButtonOk.setForeground(Color.WHITE);
 		getContentPane().add(ButtonOk);
-
+		//Ok Button END
+		
+		//Cancel Button
 		ButtonCancel = new JButton("Cancel");
-		ButtonCancel.setBounds(194, 79, 89, 23);
+		ButtonCancel.setBounds(198, 86, 99, 23);
+		ButtonCancel.setFont(new Font("Arial Black", Font.PLAIN, 13));
+		ButtonCancel.setIcon(new ImageIcon(imgOK));
+		ButtonCancel.setOpaque(false);
+		ButtonCancel.setContentAreaFilled(false);
+		ButtonCancel.setBorderPainted(false);
+		ButtonCancel.setHorizontalTextPosition(JButton.CENTER);
+		ButtonCancel.setVerticalTextPosition(JButton.CENTER);
+		ButtonCancel.setForeground(Color.WHITE);
 		getContentPane().add(ButtonCancel);
+		//Cancel Button END
+		
+		
+		//Background Image
+		JLabel ImageHere = new JLabel();
+		ImageHere.setBounds(0, 0, 358, 120);
+		Image img4 = new ImageIcon(this.getClass().getResource("/bgSmall.png")).getImage();
+		ImageHere.setIcon(new ImageIcon(img4));
+		getContentPane().add(ImageHere);
+		//Background Image END
 
 		this.setTitle("Good Reading - Initial Config");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setBounds(500, 200, 358, 155);
 		this.setVisible(true);
 		this.setResizable(false);
+		
+		
+	    
 	}
 
-	private JTextField getTextHost(){
+	/*private JTextField getTextHost(){
 		if(TextHost == null){
 			TextHost = new JTextField();
 			TextHost.setBounds(51, 9, 264, 20);
@@ -59,9 +182,9 @@ public class ClientGUI extends JFrame{
 			TextHost.setText("localhost");
 		}
 		return TextHost;
-	}
+	}*/
 
-	private JTextField getTextPort(){
+	/*private JTextField getTextPort(){
 		if(TextPort == null){
 			TextPort = new JTextField();
 			TextPort.setBounds(51, 37, 264, 20);
@@ -69,7 +192,7 @@ public class ClientGUI extends JFrame{
 			TextPort.setText("5555");
 		}
 		return TextPort;
-	}
+	}*/
 
 	public void clearFields(){
 		TextHost.setText("");
--- src/gui/LoginGUI.java
+++ src/gui/LoginGUI.java
@@ -1,16 +1,23 @@
 package gui;
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.Font;
+import java.awt.Image;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import java.awt.Toolkit;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 
 public class LoginGUI extends JFrame {
@@ -18,12 +25,14 @@ public class LoginGUI extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField userField;
+	private JTextField nullField;
 	private JTextField passwordField;
 	private JButton buttonCancel;
 	private JButton buttonLogin;
 	private JButton buttonForgotPassword;
 	private JLabel labelWarningMessage = null;
 	private JButton buttonChangePassword;
+	private JTextField txtSignIn;
 
 
 	/**
@@ -34,60 +43,131 @@ public class LoginGUI extends JFrame {
 		setTitle("Good Reading - Login");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 400, 360);
+		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		JLabel lblImageHere = new JLabel("Image Here");
-		lblImageHere.setBounds(120, 0, 156, 150);
-		//lblImageHere.setIcon(new ImageIcon(LoginGUI.class.getResource("/images/logo2.png")));
-		contentPane.add(lblImageHere);
 
-		JLabel lblUsername = new JLabel("Username:");
-		lblUsername.setFont(new Font("Arial", Font.BOLD, 16));
-		lblUsername.setBounds(76, 146, 87, 16);
-		contentPane.add(lblUsername);
-
-		JLabel lblPassword = new JLabel("Password:");
-		lblPassword.setFont(new Font("Arial", Font.BOLD, 16));
-		lblPassword.setBounds(76, 177, 87, 16);
-		contentPane.add(lblPassword);
-
-		userField = new JTextField();
-		userField.setBounds(163, 144, 139, 22);
-		contentPane.add(userField);
-		userField.setColumns(10);
-
-		passwordField = new JTextField();
+		nullField = new JTextField();
+		nullField.setOpaque(false);     
+		contentPane.add(nullField);
+		
+	
+		
+		//Passowrd Field JText
+		passwordField = new JTextField()
+		{
+				  @Override public void setBorder(Border border) {   //Disable field's border
+				      // No!
+				  }
+		};
+		passwordField.setForeground(Color.WHITE);
+		passwordField.setFont(new Font("Arial", Font.PLAIN, 13));
+		passwordField.setOpaque(false);       //Make Field transparent 
+		passwordField.setText("Password");
+		passwordField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent arg0) {
+				passwordField.setText("");                   //Clear "Password String" on focus
+			}
+		});
+		passwordField.setToolTipText("");
 		passwordField.setColumns(10);
-		passwordField.setBounds(163, 175, 139, 22);
+		passwordField.setBounds(295, 305, 224, 40);
 		contentPane.add(passwordField);
-
-		buttonLogin = new JButton("Login");
-		//buttonLogin.setIcon(new ImageIcon(LoginGUI.class.getResource("/images/login.PNG")));
-		buttonLogin.setBounds(94, 235, 97, 25);
+		//Password END
+		
+		
+		//Username field JText
+		userField = new JTextField()
+		{
+		    @Override public void setBorder(Border border) {   //Disable field's border
+		        // No!
+		    }
+		};
+		
+		userField.setForeground(Color.WHITE);
+		userField.setFont(new Font("Arial", Font.PLAIN, 13));
+		userField.setOpaque(false);       //Make Field transparent 
+		userField.setText("Username");
+		userField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				userField.setText("");                //Clear "Username String" on focus
+			}
+		});
+		
+		userField.setToolTipText("");
+		userField.setColumns(10);
+		userField.setBounds(295, 266, 224, 39);
+		contentPane.add(userField);
+		//Username field JText END
+		
+				
+		//Username and Password Lables		
+		JLabel FieldUser = new JLabel();
+		FieldUser.setBounds(292, 266, 229, 40);
+		Image img1 = new ImageIcon(this.getClass().getResource("/Label.png")).getImage();
+		FieldUser.setIcon(new ImageIcon(img1));
+		contentPane.add(FieldUser);
+		
+		JLabel FieldPass = new JLabel();
+		FieldPass.setBounds(292, 305, 229, 40);
+		Image img2 = new ImageIcon(this.getClass().getResource("/Label.png")).getImage();
+		FieldPass.setIcon(new ImageIcon(img1));
+		contentPane.add(FieldPass);
+		//Username and Password Labels END
+		
+		//Sign IN Button
+		buttonLogin = new JButton("Sign In");
+		buttonLogin.setFont(new Font("Arial", Font.BOLD, 15));
+		Image imgLogin = new ImageIcon(this.getClass().getResource("/Button.png")).getImage();
+		buttonLogin.setIcon(new ImageIcon(imgLogin));
+		buttonLogin.setBounds(292, 347, 224, 39);
+		buttonLogin.setOpaque(false);
+		buttonLogin.setContentAreaFilled(false);
+		buttonLogin.setBorderPainted(false);
+		buttonLogin.setHorizontalTextPosition(JButton.CENTER);
+		buttonLogin.setVerticalTextPosition(JButton.CENTER);
+		buttonLogin.setForeground(Color.WHITE);
 		contentPane.add(buttonLogin);
-
+		//Sign IN button END
+		
 		buttonCancel = new JButton("Cancel");
 		//buttonCancel.setIcon(new ImageIcon(LoginGUI.class.getResource("/images/cancel.PNG")));
 		buttonCancel.setBounds(203, 235, 97, 25);
-		contentPane.add(buttonCancel);
+		//contentPane.add(buttonCancel);
 
-		buttonForgotPassword = new JButton("Forgot your password?");
-		//buttonForgotPassword.setIcon(new ImageIcon(LoginGUI.class.getResource("/images/update.png")));
-		buttonForgotPassword.setForeground(Color.RED);
-		buttonForgotPassword.setBounds(94, 265, 206, 25);
-		contentPane.add(buttonForgotPassword);
+		//Forgot Password Button
+		JButton btnForgotPassword = new JButton("FORGOT PASSWORD");
+		btnForgotPassword.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		btnForgotPassword.setBounds(325, 399, 143, 25);
+		btnForgotPassword.setForeground(Color.WHITE);
+		btnForgotPassword.setOpaque(false);
+		btnForgotPassword.setContentAreaFilled(false);
+		btnForgotPassword.setBorderPainted(false);
+		btnForgotPassword.setHorizontalTextPosition(JButton.CENTER);
+		btnForgotPassword.setVerticalTextPosition(JButton.CENTER);
+		contentPane.add(btnForgotPassword);
+		//Forgot Password Button END
 
 		buttonChangePassword = new JButton("Change Password");
 		//buttonChangePassword.setIcon(new ImageIcon(LoginGUI.class.getResource("/images/Password_protection_symbol_on_monitor_screen_24.png")));
 		buttonChangePassword.setForeground(Color.BLACK);
 		buttonChangePassword.setBounds(94, 295, 206, 25);
-		contentPane.add(buttonChangePassword);
+		//contentPane.add(buttonChangePassword);
 		setLocationRelativeTo(null);
 		setVisible(true);
+		
+		//Background Image
+		JLabel lblImageHere = new JLabel();
+		lblImageHere.setBounds(0, 0, 800, 600);
+		Image img = new ImageIcon(this.getClass().getResource("/bgWithLogo.png")).getImage();
+		lblImageHere.setIcon(new ImageIcon(img));
+		contentPane.add(lblImageHere);
+		//Background Image END
 
 	}
 	// Action Listeners

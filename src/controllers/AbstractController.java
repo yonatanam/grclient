ca9a5cbf95c3857dcd0b927d7f9d6f3b06a3a2c1
emilgelm <emilgelm@emilgelm-MOBL1.ger.corp.intel.com>
2017-01-16 23:31:45 +0200

    Bug fixing

--- src/controllers/AbstractController.java
+++ src/controllers/AbstractController.java
@@ -1,8 +1,6 @@
 package controllers;
 
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
@@ -22,7 +20,6 @@ import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
 import client.Client;
-import gui.MainWindowGUI;
 import models.Envelope;
 //import client.CliMessage;
 //import client.IObserve;
@@ -35,103 +32,102 @@ import client.App;
  */
 public abstract class AbstractController 
 {
-	public static final float DEFAULT_THREAD =0;
-	/** The model. */
-	protected Object theModel;
+		public static final float DEFAULT_THREAD =0;
+		/** The model. */
+		protected Object theModel;
 
-	/**
-	 * Instantiates a new abstract controller.
-	 * @param theModel the the model
-	 * 
-	 */
-	public AbstractController(){}
-
-	public AbstractController(Object theModel) 
-	{
-		this.theModel = theModel;
-
-	}
-
-	/**
-	 * Send to server with String
-	 *
-	 * @param request the request
-	 */
-
-
-	protected void sendToServer(String request)
-	{
-		try 
+		/**
+		 * Instantiates a new abstract controller.
+		 * @param theModel the the model
+		 * 
+		 */
+		public AbstractController(){}
+		
+		public AbstractController(Object theModel) 
 		{
-			App.client.sendToServer(request);
-		} 
-		catch (IOException e)
-		{
-			JOptionPane.showMessageDialog(null,"Error: Server comunication problem","Commuinication Error", JOptionPane.ERROR_MESSAGE);
-			e.printStackTrace();
+			this.theModel = theModel;
+		
 		}
-	}
-
+		
+		/**
+		 * Send to server with String
+		 *
+		 * @param request the request
+		 */
+		
+		
+		protected void sendToServer(String request)
+		{
+			try 
+			{
+				 App.client.sendToServer(request);
+			} 
+			catch (IOException e)
+			{
+				JOptionPane.showMessageDialog(null,"Error: Server comunication problem","Commuinication Error", JOptionPane.ERROR_MESSAGE);
+				e.printStackTrace();
+			}
+		}
+		
 	/**
 	 * Send to server with Object	
 	 * @param request
 	 */
-
-
-	protected void sendToServer(Object request)
-	{
-		//System.out.println("SendToServer");
-
-		try 
+		
+		
+		protected void sendToServer(Object request)
 		{
+			//System.out.println("SendToServer");
 
-			App.client.sendToServer(request);
-		} 
-		catch (IOException e)
-		{
-			JOptionPane.showMessageDialog(null,"Error: Server comunication problem","Commuinication Error", JOptionPane.ERROR_MESSAGE);
-			e.printStackTrace();
+			try 
+			{
+				
+				 App.client.sendToServer(request);
+			} 
+			catch (IOException e)
+			{
+				JOptionPane.showMessageDialog(null,"Error: Server comunication problem","Commuinication Error", JOptionPane.ERROR_MESSAGE);
+				e.printStackTrace();
+			}
 		}
+		
+		/**
+		 * Method to handle the answer of the db.
+		 * Controller class usually needed to overwrite this method as own reqeusts.
+		 * @param message Answer of the db as Object.
+		 */
+		 public void handleDBResult(Object message) {
+			 
+			 //
+		 }
+		 
+		 public void UpdateDB(){
+			 //
+		 }
+		
+			class CustomWindowListener implements WindowListener {	      
+			      public void windowClosing(WindowEvent e) {    
+						Map<String, Object> params = new LinkedHashMap<String,Object>();
+						Envelope envelope = new Envelope(params);			
+						params.put("username",  App.client.getCurrentUser().getUserName());
+						params.put("status", "ACTIVE");
+						params.put("threadnum", DEFAULT_THREAD);
+						params.put("msg",  "UpdateUserLoginStatus");
+						sendToServer(envelope);					
+			      }	      
+			      public void windowClosed(WindowEvent e) {   	  
+			      }
+			      public void windowOpened(WindowEvent e) {	    	  
+			      }
+			      public void windowIconified(WindowEvent e) {
+			      }
+			      public void windowDeiconified(WindowEvent e) {
+			      }
+			      public void windowActivated(WindowEvent e) {
+			      }
+			      public void windowDeactivated(WindowEvent e) {
+			      }
+			   }  
+			
+		
 	}
-
-	/**
-	 * Method to handle the answer of the db.
-	 * Controller class usually needed to overwrite this method as own reqeusts.
-	 * @param message Answer of the db as Object.
-	 */
-	public void handleDBResult(Object message) {
-
-		//
-	}
-
-
-	public void UpdateDB(){
-		//
-	}
-
-	class CustomWindowListener implements WindowListener {	      
-		public void windowClosing(WindowEvent e) {    
-			Map<String, Object> params = new LinkedHashMap<String,Object>();
-			Envelope envelope = new Envelope(params);			
-			params.put("username",  App.client.getCurrentUser().getUserName());
-			params.put("status", "ACTIVE");
-			params.put("threadnum", DEFAULT_THREAD);
-			params.put("msg",  "UpdateUserLoginStatus");
-			sendToServer(envelope);					
-		}	      
-		public void windowClosed(WindowEvent e) {   	  
-		}
-		public void windowOpened(WindowEvent e) {	    	  
-		}
-		public void windowIconified(WindowEvent e) {
-		}
-		public void windowDeiconified(WindowEvent e) {
-		}
-		public void windowActivated(WindowEvent e) {
-		}
-		public void windowDeactivated(WindowEvent e) {
-		}
-	}  
-
-
-}
